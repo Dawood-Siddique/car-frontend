9363922178ff94fad6ab854023940cb1ff1be3f7
Haroon <81903831+Dawood-Siddique@users.noreply.github.com>
2025-09-22 01:55:40 +0500

    added .env

---
+++ src/services/auth.ts
@@ -0,0 +1,25 @@
+import { type Credentials } from '@/types';
+
+const API_URL = import.meta.env.VITE_BASE_URL;
+
+if (!API_URL) {
+  throw new Error('VITE_BASE_URL is not defined. Please check your .env file.');
+}
+
+export const login = async (credentials: Credentials) => {
+  const response = await fetch(`${API_URL}auth/user/login/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Login failed');
+  }
+
+  const data = await response.json();
+  return data;
+};
