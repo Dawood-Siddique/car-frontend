6c0f77a7d0aba8e58ca53a9ecce6221698860837
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-09-30 16:59:54 +0500

    Added logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
 
-import { Car } from "lucide-react";
 import { FC } from "react";
+import logo from '../assets/logo.png';
 
 const Header: FC = () => {
   return (
     <header className="bg-blue-500 text-white py-4 px-6 flex items-center justify-between">
       <div className="flex items-center">
-        <Car className="h-8 w-8 mr-2" />
+        <img src={logo} alt="Logo" className="h-8 w-8 mr-2" />
         <h1 className="text-2xl font-bold">Prestige Motors</h1>
       </div>
     </header>
